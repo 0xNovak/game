a7f7c4f4ac2e5007c55c8fb64f90bf2071a0026a
0xNovak <nowackiwik@gmail.com>
2026-05-10 12:44:04 +0200

    added basic assetmanager
    added struct for maintaining textures for sprites

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,16 +1,22 @@
 #include "engine.h"
 
-#include <chrono>
+#include <cstdint>
+#include <ctime>
 #include <iostream>
+#include <map>
 #include <mutex>
+#include <stdexcept>
 #include <thread>
 
 #include "SFML/Graphics/RenderWindow.hpp"
 #include "SFML/Graphics/Sprite.hpp"
 #include "SFML/Graphics/Texture.hpp"
+#include "SFML/System/Clock.hpp"
 #include "SFML/System/Sleep.hpp"
+#include "SFML/System/Time.hpp"
 #include "SFML/Window/VideoMode.hpp"
 #include "SFML/Window/WindowEnums.hpp"
+#include "engine/render_entries.h"
 #include "engine/renderer.h"
 
 using namespace sgr;
@@ -31,15 +37,41 @@ void Engine::engineInit() {
   m_window.setPosition({0, 0});
 }
 void Engine::loopUpdate() {
-  // -- test --
-  // const sf::Texture txt_TMP{"resources/gfx/tmp.png"};
-  // {
-  //   render::RenderEntry entry{0, sf::Sprite{txt_TMP}};
-  //   updateSnap->addEntry(entry);
-  // }
-  // updateSnap->vec[0].sprite.setPosition({200.f, 200.f});
-  while (m_IsRunning) {
+  std::unordered_map<uint16_t, const sf::Texture> hashMap{};
+  hashMap.insert({0, sf::Texture{"resources/gfx/tmp.png"}});
+  const sf::Texture txt_TMP{"resources/gfx/tmp.png"};
 
+  sf::Clock clock;
+  const auto delay = 1'000'000 / UPDATERATE;
+  int64_t nextUpdate = delay;
+  int64_t lastUpdate = 0;
+  auto PositionY = 100.f;
+  while (m_IsRunning) {
+    sf::sleep(
+        sf::microseconds(nextUpdate - clock.getElapsedTime().asMicroseconds()));
+    auto now = clock.getElapsedTime().asMicroseconds();
+    nextUpdate = now + delay;
+    auto deltaTime = now - lastUpdate;
+    lastUpdate = now;
+
+    updateSnap->vec.clear();
+    update(deltaTime);
+    PositionY += .5f;
+    if (PositionY > 200.f)
+      PositionY = 100.f;
+
+    auto txt_it = hashMap.find(0);
+    if (txt_it == hashMap.end()) {
+      m_IsRunning.store(false);
+      throw std::runtime_error("txt 0 not found");
+    }
+
+    updateSnap->addEntry({0, sf::Sprite{txt_it->second}});
+    updateSnap->addEntry({1, sf::Sprite{txt_TMP}});
+    updateSnap->addEntry({2, sf::Sprite{txt_TMP}});
+    updateSnap->vec[0].sprite.setPosition({20, 100});
+    updateSnap->vec[0].sprite.setPosition({50, 100});
+    updateSnap->vec[2].sprite.setPosition({130, PositionY});
     {
       std::lock_guard<std::mutex> lock{snapMutex};
       std::swap(updateSnap, renderSnap);
