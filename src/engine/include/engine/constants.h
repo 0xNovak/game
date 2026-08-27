62801feada0c2ca8a4899640db815b0808cc6a73
0xNovak <181787300+0xNovak@users.noreply.github.com>
2026-08-28 00:49:16 +0200

    changed framerate&updaterate

--- src/engine/include/engine/constants.h
+++ src/engine/include/engine/constants.h
@@ -3,7 +3,7 @@
 #include "SFML/System/Vector2.hpp"
 #include <cstdint>
 namespace sgr::constants {
-constexpr uint32_t FRAMERATE = 120;  // per sec
-constexpr uint32_t UPDATERATE = 144; // per sec
+constexpr uint32_t FRAMERATE = 180;  // per sec
+constexpr uint32_t UPDATERATE = 240; // per sec
 constexpr sf::Vector2u WINDOW_SIZE{1280, 720};
 }; // namespace sgr::constants
